4388d1b2d764ad75f2fe15ecac89f2bbb93de36d
thanujad <43904883+thanujad@users.noreply.github.com>
2019-07-02 21:01:19 +0530

    upload

---
+++ ExcepDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Class_Demos
+{
+    class ExcepDemo
+    {
+        static void Main(string[] args)
+        {
+            int div=0, number=0;
+            try
+            {
+                div = 100 / number;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Exception occured : " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Result is: {0}", div);
+                Console.WriteLine("\nPress ENTER to Quit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
